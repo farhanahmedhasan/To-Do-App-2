7f2bf7e09ffbe3c996ca769f1e7b6f744bf944af
Hasan <aaxhasandjmamunxyz2015@gmail.com>
2021-01-06 23:36:04 +0600

    Design and Some Funtionality is Complete

---
+++ app/assests/scripts/app.js
@@ -0,0 +1,61 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+const todotext = document.querySelector('.todo-text')
+
+//Event Listener
+todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteCheck)
+
+//Functions
+//Function for create li
+function addTodo(event) {
+  //Prevent Form from being Submitted
+	event.preventDefault()
+	
+	//Create ToDo Div
+	const toDoDiv = document.createElement('div') //created a Div
+	toDoDiv.classList.add('todo') //Added a class
+
+	//Create li
+	const toDoText = document.createElement('li') //Created li 
+	toDoText.classList.add('todo-text') //Added a class
+	toDoText.innerText = todoInput.value
+	todoInput.value = ''
+
+	toDoDiv.appendChild(toDoText) //Put li inside a div
+
+	//CheckMark Button
+	const completedButton = document.createElement('button') //Created Button
+	completedButton.classList.add('complete') //Added Class
+	completedButton.innerHTML = `<i class="fas fa-check"></i>` //Seted Icon
+	toDoDiv.appendChild(completedButton) //Put it inside Parent Div
+
+		//Delete Button
+	const deleteButton = document.createElement('button') //Created Button
+	deleteButton.classList.add('delete') //Added Class
+	deleteButton.innerHTML = `<i class="fas fa-trash"></i>` //Seted Icon
+	toDoDiv.appendChild(deleteButton) //Put it inside Parent Div
+
+	//Append The Div inside the Ul
+	todoList.appendChild(toDoDiv)
+}
+
+//Function for delete n Check
+function deleteCheck(e) {
+	const element = e.target
+
+	//Delete Todo
+	if (element.classList[0] === 'delete') {
+		element.parentElement.classList.add('fall')
+		setTimeout(() => {
+			element.parentElement.remove()
+		},300)
+	}
+
+	//Completed Todo
+	if (element.classList[0] === 'complete') {
+		element.parentElement.classList.toggle('completed')
+	}
+}
